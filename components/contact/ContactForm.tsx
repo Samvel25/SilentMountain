449c5ff1a631eeea318905369f7e0f904a23f0b5
Samvel Avagyan <48676414+Samvel25@users.noreply.github.com>
2024-11-15 14:42:09 +0400

    fix

--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -369,7 +369,7 @@ const ContactForm: React.FC<ContactFormProps> = ({
 
 		try {
 			const response = await fetch(
-				"https://script.google.com/macros/s/AKfycbzvP7BcJfMJNbGsm7p3UBdsHs3F_J3gIcNw6xamS3UMTIkslhSWXKMJZDZVsKy-5gJG/exec", // Replace with your Google Apps Script URL
+				"https://script.google.com/macros/s/AKfycbw_2jCq2lntdjIEWTjigAdzSIZgV_HoUgNc1M0NuUI88ZIH0wh8XZLFWfo0m_GMD-mXHA/exec", // Replace with your Google Apps Script URL
 				{
 					method: "POST",
 					headers: {
