0a2a7704c965fb058cc0be66b902f3fba0ca109a
Samvel Avagyan <48676414+Samvel25@users.noreply.github.com>
2024-11-18 15:15:09 +0400

    Admine changes

--- components/blog/BlogSidebar.tsx
+++ components/blog/BlogSidebar.tsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import { blogs } from "@/public/data/blogs";
 import { useRouter, usePathname } from "next/navigation";
 import Image from "next/image";
+import { Blog } from "@/types/types";
 
 interface Category {
 	name: string;
@@ -16,6 +16,7 @@ interface BlogSidebarProps {
 	initialCategory?: string;
 	currentBlogId?: number;
 	showRelatedArticles?: boolean;
+	blogs: Blog[];
 }
 
 const BlogSidebar = ({
@@ -25,6 +26,7 @@ const BlogSidebar = ({
 	initialCategory = "All",
 	currentBlogId,
 	showRelatedArticles = false,
+	blogs,
 }: BlogSidebarProps) => {
 	const router = useRouter();
 	const pathname = usePathname();
@@ -35,67 +37,17 @@ const BlogSidebar = ({
 	const [x, setX] = useState(0);
 	const [y, setY] = useState(0);
 
-	useEffect(() => {
-		// Extract search term from URL query parameters on component mount
-		const urlParams = new URLSearchParams(window.location.search);
-		const searchParam = urlParams.get("search");
-		if (searchParam) {
-			setSearchTerm(searchParam);
-		}
-	}, []);
-
 	// Generate categories from blogs data
 	const categories: Category[] = [
 		{ name: "All", count: blogs.length },
-		{
-			name: "App",
-			count: blogs.filter((blog) => blog.tags.includes("App")).length,
-		},
-		{
-			name: "Game App",
-			count: blogs.filter((blog) => blog.tags.includes("Game App")).length,
-		},
-		{
-			name: "Game Art",
-			count: blogs.filter((blog) => blog.tags.includes("Game Art")).length,
-		},
-		{
-			name: "Game Animation",
-			count: blogs.filter((blog) => blog.tags.includes("Game Animation"))
-				.length,
-		},
-		{
-			name: "Game QA Testing",
-			count: blogs.filter((blog) => blog.tags.includes("Game QA Testing"))
-				.length,
-		},
-		{
-			name: "Unity Development",
-			count: blogs.filter((blog) => blog.tags.includes("Unity Development"))
-				.length,
-		},
-		{
-			name: "3D ART",
-			count: blogs.filter((blog) => blog.tags.includes("3D ART")).length,
-		},
-		{
-			name: "3D Game Modeling",
-			count: blogs.filter((blog) => blog.tags.includes("3D Game Modeling"))
-				.length,
-		},
-		{
-			name: "3D Animation",
-			count: blogs.filter((blog) => blog.tags.includes("3D Animation")).length,
-		},
-		{
-			name: "Environment Design",
-			count: blogs.filter((blog) => blog.tags.includes("Environment Design"))
-				.length,
-		},
-		{
-			name: "3D Characters",
-			count: blogs.filter((blog) => blog.tags.includes("3D Characters")).length,
-		},
+		...Array.from(
+			blogs.reduce((acc, blog) => {
+				blog.tags.forEach((tag) => {
+					acc.set(tag.tags, (acc.get(tag.tags) || 0) + 1);
+				});
+				return acc;
+			}, new Map<string, number>())
+		).map(([name, count]) => ({ name, count })),
 	];
 
 	const handleMouseMove = (e: React.MouseEvent) => {
@@ -111,7 +63,6 @@ const BlogSidebar = ({
 	const handleSearch = (value: string) => {
 		setSearchTerm(value);
 		if (!isDetailPage) {
-			// If we're on the blogs listing page, search immediately
 			onSearch?.(value);
 			router.push(`/blog?search=${encodeURIComponent(value)}`);
 		}
@@ -119,28 +70,23 @@ const BlogSidebar = ({
 
 	const handleSearchClick = () => {
 		if (searchTerm) {
-			// Include search term in the URL query parameters
 			router.push(`/blog?search=${encodeURIComponent(searchTerm)}`);
 		}
 	};
 
 	const handleCategoryClick = (category: string) => {
-		// Prevent default scroll behavior
 		const currentScroll = window.scrollY;
 
 		setActiveCategory(category);
 		onCategoryChange?.(category);
 
-		// Use replace instead of push to avoid adding to history stack
 		router.replace(`/blog?category=${encodeURIComponent(category)}`, {
-			scroll: false, // Prevent automatic scrolling
+			scroll: false,
 		});
 
-		// Restore scroll position
 		window.scrollTo(0, currentScroll);
 	};
 
-	// Get related articles based on current blog's tags
 	const getRelatedArticles = () => {
 		if (!currentBlogId) return [];
 		const currentBlog = blogs.find((blog) => blog.id === currentBlogId);
@@ -149,10 +95,10 @@ const BlogSidebar = ({
 		return blogs
 			.filter(
 				(blog) =>
-					blog.id !== currentBlogId && // Exclude current blog
-					blog.tags.some((tag) => currentBlog.tags.includes(tag)) // Match any tag
+					blog.id !== currentBlogId &&
+					blog.tags.some((tag) => currentBlog.tags.includes(tag))
 			)
-			.slice(0, 3); // Limit to 3 related articles
+			.slice(0, 3);
 	};
 
 	return (
@@ -232,30 +178,15 @@ const BlogSidebar = ({
 									className="single-box d-center justify-content-start gap-5"
 								>
 									<div className="img-area w-50">
-										<Image src={article.img} alt={article.title} />
+										<Image
+											src={article.image[0].formats.thumbnail.url}
+											alt={article.title}
+										/>
 									</div>
 									<div className="info-area">
-										<Link href={`/blog/${article.id}`}>
+										<Link href={`/blog/${article.slug}`}>
 											<h6>{article.title}</h6>
 										</Link>
-										{/* <div className="article-info pt-3 d-flex gap-5">
-											<div className="d-flex gap-1 single">
-												<div className="icon-wrap d-flex align-items-end">
-													<i className="material-symbols-outlined mat-icon fs-six">
-														schedule
-													</i>
-												</div>
-												<span>15 Min</span>
-											</div>
-											<div className="d-flex gap-1 single ps-5">
-												<div className="icon-wrap d-flex align-items-end">
-													<i className="material-symbols-outlined mat-icon fs-six">
-														visibility
-													</i>
-												</div>
-												<span>55</span>
-											</div>
-										</div> */}
 									</div>
 								</div>
 							))}
@@ -305,8 +236,6 @@ const BlogSidebar = ({
 						</li>
 					</ul>
 				</div>
-
-				{/* Contact Section */}
 			</div>
 		</div>
 	);
--- components/blog/Blogs.tsx
+++ components/blog/Blogs.tsx
@@ -1,16 +1,29 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect } from "react";
-import { blogs } from "@/public/data/blogs";
+import { fetchBlogs } from "@/utils/api";
 import BlogSidebar from "./BlogSidebar";
 import { useSearchParams } from "next/navigation";
+import { Blog } from "@/types/types";
 
 const Blogs = () => {
 	const searchParams = useSearchParams();
-	const [searchTerm, setSearchTerm] = useState("");
+	const initialSearchTerm = searchParams?.get("search") || "";
+	const [searchTerm, setSearchTerm] = useState(initialSearchTerm);
 	const [activeCategory, setActiveCategory] = useState("All");
+	const [blogs, setBlogs] = useState<Blog[]>([]);
 
-	// Update states based on URL parameters
+	useEffect(() => {
+		const getBlogs = async () => {
+			const data = await fetchBlogs();
+			console.log("Fetched Blogs:", data);
+			setBlogs(data);
+		};
+
+		console.log("useEffect triggered");
+		getBlogs();
+	}, []);
+
 	useEffect(() => {
 		const search = searchParams?.get("search");
 		const category = searchParams?.get("category");
@@ -19,13 +32,22 @@ const Blogs = () => {
 		if (category) setActiveCategory(category);
 	}, [searchParams]);
 
-	// Filter the blogs based on active category and search term
 	const filteredBlogs = blogs.filter((blog) => {
+		const { title, description, tags } = blog;
+
 		const matchesCategory =
-			activeCategory === "All" || blog.tags.includes(activeCategory);
+			activeCategory === "All" ||
+			tags.some((tag) => tag.tags === activeCategory);
 		const matchesSearch =
-			blog.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-			blog.description.toLowerCase().includes(searchTerm.toLowerCase());
+			title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+			description.some((desc) =>
+				desc.children.some((child) =>
+					child.text.toLowerCase().includes(searchTerm.toLowerCase())
+				)
+			);
+		console.log("useIframe:", blog.useIframe);
+		console.log("Image URL:", blog.image[0]?.formats.large.url);
+		console.log("Blog Slug:", blog.slug);
 
 		return matchesCategory && matchesSearch;
 	});
@@ -35,7 +57,6 @@ const Blogs = () => {
 			<div className="container pt-120">
 				<div className="row">
 					<div className="col-xl-8 col-lg-7">
-						{/* Show active filters if any */}
 						{(searchTerm || activeCategory !== "All") && (
 							<div className="filter-info mb-4">
 								{searchTerm && (
@@ -52,39 +73,39 @@ const Blogs = () => {
 						)}
 
 						{filteredBlogs.length > 0 ? (
-							filteredBlogs.map(({ id, title, slug, details }) => (
-								<div key={id} className="single-box">
+							filteredBlogs.map((blog) => (
+								<div key={blog.id} className="single-box">
 									<div className="position-relative d-grid align-items-center">
 										<div className="img-box">
-											{details.useIframe ? (
+											{blog.useIframe ? (
 												<iframe
 													frameBorder="0"
-													src={details.videoUrl}
+													src={blog.videoUrl}
 													allowFullScreen
 													width="975"
 													height="670"
 												></iframe>
 											) : (
-												<Image
-													src={details.blogImage}
-													className="w-100"
-													alt={title}
-												/>
+												blog.image.length > 0 && (
+													<Image
+														src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${blog.image[0].formats.large.url}`}
+														className="w-100"
+														alt={blog.title}
+														width={1000}
+														height={560}
+													/>
+												)
 											)}
 										</div>
 										<div className="position-absolute cus-position bottom-0 start-0">
 											<div className="content-box p-3 p-sm-6">
-												<div className="top-bar d-flex align-items-center gap-3">
-													{/* <h5>News</h5>
-													<span>25 January, 2023</span> */}
-												</div>
-												<Link href={`/blog/${slug}`}>
+												<Link href={`/blog/${blog.slug}`}>
 													<h3 className="visible-slowly-bottom mt-3">
-														{title}
+														{blog.title}
 													</h3>
 												</Link>
 												<Link
-													href={`/blog/${slug}`}
+													href={`/blog/${blog.slug}`}
 													className="end-area mt-8 d-center"
 												>
 													<i className="material-symbols-outlined">call_made</i>
@@ -105,6 +126,7 @@ const Blogs = () => {
 							onCategoryChange={setActiveCategory}
 							initialSearchTerm={searchTerm}
 							initialCategory={activeCategory}
+							blogs={blogs}
 						/>
 					</div>
 				</div>
